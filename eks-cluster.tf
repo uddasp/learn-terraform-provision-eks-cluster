6da09e8b06dc4aac162215711228f111dd171022
root <root@ip-172-31-0-83.ec2.internal>
2021-11-02 06:39:02 +0000

    Modified code

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -24,13 +24,13 @@ module "eks" {
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
-    {
+/*    {
       name                          = "worker-group-2"
       instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
-    },
+    }, */
   ]
 }
 
